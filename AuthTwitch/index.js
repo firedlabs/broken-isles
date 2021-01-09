a3b1cb42f90b8bbf6e130ab93718e3f120927c7a
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-09 14:08:42 +0100

    :bug: Add queue in AuthTwitch

---
+++ AuthTwitch/index.js
@@ -0,0 +1,77 @@
+const querystring = require("querystring");
+const axios = require("axios");
+const { access } = require("fs");
+const { CLIENT_SECRET, CLIENT_ID, HOST_API, HOST_FRONTEND } = process.env;
+const scope = "user:read:email";
+
+const getCodeRedirect = (context) => {
+  const query = {
+    client_id: CLIENT_ID,
+    redirect_uri: `${HOST_API}/api/auth/twitch`,
+    response_type: "code",
+    scope,
+  };
+  const url = "https://id.twitch.tv/oauth2/authorize";
+
+  context.res = {
+    status: 302,
+    headers: {
+      location: `${url}?${querystring.stringify(query, "&")}`,
+    },
+  };
+};
+
+const newAuthAddQueueWithIpAndToken = (context, token, ip) => {
+  context.bindings.newAuthTwitch = {
+    token,
+    ip,
+  };
+};
+
+const getToken = async (context, code) => {
+  const url = "https://id.twitch.tv/oauth2/token";
+  const query = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: `${HOST_FRONTEND}`,
+    code,
+    grant_type: "authorization_code",
+  };
+  let res = false;
+
+  try {
+    res = await axios.post(url, querystring.stringify(query));
+    newAuthAddQueueWithIpAndToken(context, res.data.access_token, "10.10.10.0");
+  } catch (err) {
+    const { status, message } = err.response.data;
+
+    context.res = {
+      status,
+      body: {
+        message,
+      },
+    };
+  }
+
+  if (res.data.access_token) {
+    const { access_token } = res.data;
+
+    context.res = {
+      status: 302,
+      headers: {
+        location: HOST_FRONTEND,
+      },
+      cookies: [{ name: "token", value: access_token, path: "/" }],
+    };
+  }
+};
+
+module.exports = async function (context, req) {
+  const code = req.query.code || false;
+
+  if (code) {
+    await getToken(context, code);
+  } else {
+    getCodeRedirect(context);
+  }
+};
