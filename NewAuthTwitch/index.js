0087da912e761fb93e696b7abcc98ad9a375938e
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-11 16:10:40 +0100

    :construction: Get twitch user infos

--- NewAuthTwitch/index.js
+++ NewAuthTwitch/index.js
@@ -1,3 +1,30 @@
+const axios = require("axios");
+const { CLIENT_ID } = process.env;
+
 module.exports = async function (context, myQueueItem) {
-  context.log("Chegou infos na fila newauthtwitch", myQueueItem);
+  const token = myQueueItem;
+  const url = "https://api.twitch.tv/helix/users";
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Client-Id": CLIENT_ID,
+  };
+
+  try {
+    const res = await axios.get(url, { headers });
+    const {
+      login,
+      email,
+      broadcaster_type,
+      profile_image_url,
+    } = res.data.data[0];
+
+    const user = {
+      login,
+      email,
+      brodcasterType: broadcaster_type,
+      profileImageUrl: profile_image_url,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
