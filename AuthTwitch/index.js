b69c00837b9b11921453a3c58c023c4ac190fdf6
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-15 01:27:24 +0100

    :zap: Add samesite with none

--- AuthTwitch/index.js
+++ AuthTwitch/index.js
@@ -60,6 +60,7 @@ const getToken = async (context, code) => {
           value: access_token,
           path: "/",
           secure: true,
+          SameSite: "none",
         },
       ],
     };
