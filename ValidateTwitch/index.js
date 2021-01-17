17230462157c0ec57f14e2274b8d8d27d2ba26d6
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-17 14:12:16 +0100

    :zap: Create ValidateTwitch

---
+++ ValidateTwitch/index.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+const getCookie = (cookies, name) => {
+  const search = new RegExp(`^ ${name}=|${name}=`);
+
+  const findCookie = cookies.split(";").find((item) => item.match(search));
+
+  return Boolean(findCookie) && findCookie.replace(search, "");
+};
+
+module.exports = async function (context, req) {
+  const cookies = req.headers.cookie;
+  const token = getCookie(cookies, "token");
+  const url = "https://id.twitch.tv/oauth2/validate";
+  const headers = {
+    Authorization: `OAuth ${token}`,
+  };
+  const headersResponse = {
+    "Access-Control-Allow-Credentials": true,
+  };
+
+  try {
+    const res = await axios.get(url, { headers });
+
+    return {
+      status: 200,
+      headers: headersResponse,
+    };
+  } catch (err) {
+    const { status, message } = err.response.data;
+
+    return {
+      status,
+      body: message,
+      headers: headersResponse,
+    };
+  }
+};
