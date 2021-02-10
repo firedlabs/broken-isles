b826c0cba118d03a0c8ab8ff83ce2f6bd9dd6949
Marco Bruno <marco.bruno.br@gmail.com>
2021-02-10 12:28:25 +0100

    :sparkles: Create the GetCourse function

---
+++ GetCourses/index.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const { HOST_API, KEY_FUNC_AUTHORIZED } = process.env;
+
+const headersResponse = {
+  "Access-Control-Allow-Credentials": true,
+};
+
+const getCookie = (cookies, name) => {
+  const search = new RegExp(`^ ${name}=|${name}=`);
+
+  const findCookie = cookies.split(";").find((item) => item.match(search));
+
+  return Boolean(findCookie) && findCookie.replace(search, "");
+};
+
+const hasAuthorized = async (context, req) => {
+  try {
+    const cookies = req.headers.cookie;
+    const token = getCookie(cookies, "token");
+    const url = `${HOST_API}/api/func/user/authorized/${token}?code=${KEY_FUNC_AUTHORIZED}`;
+    await axios.get(url);
+
+    return true;
+  } catch (err) {
+    context.res = {
+      status: 403,
+      headers: headersResponse,
+    };
+  }
+};
+
+const getCourses = (context, req) => {
+  try {
+    const courses = context.bindings.inputCourses;
+
+    context.res = {
+      status: 200,
+      body: {
+        courses,
+      },
+      headers: headersResponse,
+    };
+  } catch (err) {
+    context.res = {
+      status: 403,
+      headers: headersResponse,
+    };
+  }
+};
+
+module.exports = async function (context, req) {
+  (await hasAuthorized(context, req)) && getCourses(context, req);
+};
