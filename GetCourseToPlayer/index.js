1315d82c0c2021964c163b0375d9c6ff4b3373af
Marco Bruno <marco.bruno.br@gmail.com>
2021-02-13 01:15:35 +0100

    :construction: Create the GetCourseToPlayer

---
+++ GetCourseToPlayer/index.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const { HOST_API, KEY_FUNC_AUTHORIZED } = process.env;
+
+const headersResponse = {
+  "Access-Control-Allow-Credentials": true,
+};
+
+const getCookie = (cookies, name) => {
+  const search = new RegExp(`^ ${name}=|${name}=`);
+
+  const findCookie = cookies.split(";").find((item) => item.match(search));
+
+  return Boolean(findCookie) && findCookie.replace(search, "");
+};
+
+const hasAuthorized = async (context, req) => {
+  try {
+    const cookies = req.headers.cookie;
+    const token = getCookie(cookies, "token");
+    const url = `${HOST_API}/api/func/user/authorized/${token}?code=${KEY_FUNC_AUTHORIZED}`;
+    await axios.get(url);
+
+    return true;
+  } catch (err) {
+    context.res = {
+      status: 403,
+      headers: headersResponse,
+    };
+  }
+};
+
+const getCourseToPlay = (context, req) => {
+  context.res = {
+    status: 200,
+    body: {
+      value: "vai que vai",
+    },
+    headers: headersResponse,
+  };
+};
+
+module.exports = async function (context, req) {
+  (await hasAuthorized(context, req)) && getCourseToPlay(context, req);
+};
